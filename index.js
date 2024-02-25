f96945140c706008542b4f05a8aeb1b56eb0a77d
Trisout <81978700+trisout78@users.noreply.github.com>
2024-02-25 12:34:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const client = new Discord.Client();
 const channelId = '1211267248669458432'; 
 const userId = '827232338039144518'; 
 
-const offlineMessages = ['Arcade viens de deco 😭. Surement pour faire du renforcement musculaire ', 'Arcade viens de deco 😭! Il a surement oublié que discord était sur son navigateur internet ⚡', 'Arcade viens de deco 😭! Son modem 56k a crash', 'Arcade viens de deco 😭! Il a lancé 2 minecraft sur son ordi et il a tout fait crash', 'Arcade a deco 😭! Il a oublié de faire ses devoirs.'];
-const onlineMessages = ['ARCADE VIENS DE SE CONNECTER !!! ALLEZ LE SPAMMMMMMMMMMMMMMMMM MAINTENANT', 'Arcade viens de se co! Il a rallumé son modem 56k', 'Arcade viens de se co ! Il a fallit oublié Etercube !', 'Arcade viens de se co ! Il viens surement de se faire quitter', 'Arcade viens de se co ! Il viens de déclarer Etercube au Gabon.'];
+const offlineMessages = ['Arcade vient de deco 😭. Surement pour faire du renforcement musculaire ', 'Arcade vient de deco 😭! Il a surement oublié que discord était sur son navigateur internet', 'Arcade vient de deco 😭! Son modem 56k a crash', 'Arcade vient de deco 😭! Il a lancé 2 minecraft sur son ordi et il a tout fait crash', 'Arcade vient deco 😭! Il a oublié de faire ses devoirs.'];
+const onlineMessages = ['ARCADE VIENT DE SE CONNECTER !!! ALLEZ LE SPAMMMMMMMMMMMMMMMMM MAINTENANT', 'Arcade vient de se co! Il a rallumé son modem 56k', 'Arcade vient de se co ! Il a fallit oublié Etercube !', 'Arcade vient de se co ! Il vient surement de se faire quitter', 'Arcade vient de se co ! Il vient de déclarer Etercube au Gabon.'];
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -32,4 +32,4 @@ process.on('uncaughtException', function (err) {
 });
 
 
-client.login('MTIxMDk1OTc2MTc4NTQ5MTQ4Ng.GoB99V.LMMlJzL6rWLGaw2MBy8CJsdFIDPVNEViuI1olQ'); 
+client.login('token'); 
